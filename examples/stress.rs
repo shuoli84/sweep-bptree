424f4a6f1430e92021f77de81dd30eada0b19397
shuo <shuoli84@gmail.com>
2023-03-29 00:36:09 +0800

    add delete in stress

--- examples/stress.rs
+++ examples/stress.rs
@@ -5,7 +5,8 @@ use sweep_bptree::{BPlusTree, NodeStoreVec};
 
 type NodeStore = NodeStoreVec<Point, Value, 64, 65, 64>;
 
-fn main() {
+#[inline(never)]
+fn create_tree() -> BPlusTree<NodeStore> {
     let node_store = NodeStore::new();
     let mut tree = BPlusTree::new(node_store);
 
@@ -16,5 +17,24 @@ fn main() {
         };
         tree.insert(k, Value::default());
     }
+
     println!("{}", tree.len());
+    tree
+}
+
+#[inline(never)]
+fn delete_tree(tree: &mut BPlusTree<NodeStore>) {
+    for i in 0..1000000 {
+        let k = Point {
+            x: i as f64,
+            y: i as f64,
+        };
+        tree.remove(&k);
+    }
+    println!("{}", tree.len());
+}
+
+fn main() {
+    let mut tree = create_tree();
+    delete_tree(&mut tree);
 }
